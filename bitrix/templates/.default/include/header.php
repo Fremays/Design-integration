8cea4cdc7142c5b179349ca68cb1d05da4b7505c
Frem <Shchale@yandex.ru>
2019-04-23 16:08:40 +0300

    Одинаковые области перенесены в дефолт

---
+++ bitrix/templates/.default/include/header.php
@@ -0,0 +1,88 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+IncludeTemplateLangFile(__FILE__);
+?>
+
+<div class="hd_header">
+    <table>
+        <tr>
+            <td rowspan="2" class="hd_companyname">
+                <h1>
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:main.include",
+                        ".default",
+                        Array(
+                            "AREA_FILE_SHOW" => "file",
+                            "COMPONENT_TEMPLATE" => ".default",
+                            "EDIT_TEMPLATE" => "",
+                            "PATH" => "/include/logo.php"
+                        )
+                    );?>
+                </h1>
+            </td>
+            <td rowspan="2" class="hd_txarea">
+                        <span class="tel">
+                                                        <?$APPLICATION->IncludeComponent(
+                                                            "bitrix:main.include",
+                                                            ".default",
+                                                            Array(
+                                                                "AREA_FILE_SHOW" => "file",
+                                                                "COMPONENT_TEMPLATE" => ".default",
+                                                                "EDIT_TEMPLATE" => "",
+                                                                "PATH" => "/include/phone.php"
+                                                            )
+                                                        );?>
+                        </span>	<br/>
+                <?=GetMessage('WORKING_TIME')?><span class="workhours"> ежедневно с 9-00 до 18-00</span>
+            </td>
+            <td style="width:232px">
+                <form action="">
+                    <div class="hd_search_form" style="float:right;">
+                        <input placeholder="Поиск" type="text"/>
+                        <input type="submit" value=""/>
+                    </div>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td style="padding-top: 11px;">
+							<span class="hd_singin"><a id="hd_singin_but_open" href="">Войти на сайт</a>
+							<div class="hd_loginform">
+								<span class="hd_title_loginform">Войти на сайт</span>
+								<form name="" method="" action="">
+
+									<input placeholder="Логин"  type="text">
+									<input  placeholder="Пароль"  type="password">
+									<a href="/" class="hd_forgotpassword">Забыли пароль</a>
+
+									<div class="head_remember_me" style="margin-top: 10px">
+										<input id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" type="checkbox">
+										<label for="USER_REMEMBER_frm" title="Запомнить меня на этом компьютере">Запомнить меня</label>
+									</div>
+									<input value="Войти" name="Login" style="margin-top: 20px;" type="submit">
+									</form>
+								<span class="hd_close_loginform">Закрыть</span>
+							</div>
+							</span><br>
+                <a href="" class="hd_signup">Зарегистрироваться</a>
+            </td>
+        </tr>
+    </table>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:menu",
+        "top_multi",
+        Array(
+            "ALLOW_MULTI_SELECT" => "N",
+            "CHILD_MENU_TYPE" => "left",
+            "COMPONENT_TEMPLATE" => "horizontal_multilevel",
+            "DELAY" => "N",
+            "MAX_LEVEL" => "2",
+            "MENU_CACHE_GET_VARS" => "",
+            "MENU_CACHE_TIME" => "3600",
+            "MENU_CACHE_TYPE" => "N",
+            "MENU_CACHE_USE_GROUPS" => "Y",
+            "ROOT_MENU_TYPE" => "top",
+            "USE_EXT" => "N"
+        )
+    );?>
+</div>
--- bitrix/templates/inner/footer.php
+++ bitrix/templates/inner/footer.php
@@ -44,6 +44,17 @@ IncludeTemplateLangFile(__FILE__);
         <h5><a href="">Мебельная полка всего за 560 Р</a></h5>
         <a href="" class="sb_action_more">Подробнее &rarr;</a>
     </div>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:main.include",
+        "",
+        Array(
+            "AREA_FILE_RECURSIVE" => "Y",
+            "AREA_FILE_SHOW" => "sect",
+            "AREA_FILE_SUFFIX" => "inc",
+            "EDIT_TEMPLATE" => "",
+            "PATH" => "/include/logo.php"
+        )
+    );?>
     <div class="sb_reviewed">
         <img src="/bitrix/templates/.default/content/8.png" class="sb_rw_avatar" alt=""/>
         <span class="sb_rw_name">Сергей Антонов</span>
@@ -57,57 +68,7 @@ IncludeTemplateLangFile(__FILE__);
 </div>
 </div>
 
-<div class="ft_footer">
-    <div class="ft_container">
-        <div class="ft_about">
-            <h4>О магазине</h4>
-            <ul>
-                <li><a href="">Отзывы</a></li>
-                <li><a href="">Контакты</a></li>
-                <li><a href="">Руководство</a></li>
-                <li><a href="">История</a></li>
-            </ul>
-        </div>
-        <div class="ft_catalog">
-            <h4>Каталог товаров</h4>
-            <ul>
-                <li><a href="">Кухни</a></li>
-                <li><a href="">Кровати и кушетки</a></li>
-                <li><a href="">Гарнитуры</a></li>
-                <li><a href="">Тумобчки и прихожие</a></li>
-                <li><a href="">Спальни и матрасы</a></li>
-                <li><a href="">Аксессуары</a></li>
-                <li><a href="">Столы и стулья</a></li>
-                <li><a href="">Каталоги мебели</a></li>
-                <li><a href="">Раскладные диваны</a></li>
-                <li><a href="">Кресла</a></li>
-            </ul>
-
-        </div>
-        <div class="ft_contacts">
-            <h4><?=GetMessage("CONTACT_INFO")?></h4>
-            <!-- vCard        http://help.yandex.ru/webmaster/hcard.pdf      -->
-            <p class="vcard">
-						<span class="adr">
-							<span class="street-address">ул. Летняя стр.12, офис 512</span>
-						</span>
-                <span class="tel">8 (495) 212-85-06</span>
-                <strong><?=GetMessage('WORKING_TIME')?>:</strong> <br/> <span class="workhours">ежедневно с 9-00 до 18-00</span><br/>
-            </p>
-            <ul class="ft_solcial">
-                <li><a href="" class="fb"></a></li>
-                <li><a href="" class="tw"></a></li>
-                <li><a href="" class="ok"></a></li>
-                <li><a href="" class="vk"></a></li>
-            </ul>
-            <div class="ft_copyright">© 2000 - 2012 "Мебельный магазин" </div>
-
-
-        </div>
-
-        <div class="clearboth"></div>
-    </div>
-</div>
+<?include_once($_SERVER['DOCUMENT_ROOT']."/bitrix/templates/.default/include/footer.php");?>
 </div>
 </body>
 </html>
