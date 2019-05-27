4cff6009ab4a5415bbd9ca16b6080c375d111a7c
Frem <Shchale@yandex.ru>
2019-05-27 21:05:28 +0300

    Возможность добовлять и удалять новости

--- bitrix/templates/inner/components/bitrix/news.list/review/template.php
+++ bitrix/templates/inner/components/bitrix/news.list/review/template.php
@@ -13,6 +13,11 @@
 $this->setFrameMode(true);
 ?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+<div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 <div class="review-block">
     <div class="review-text">
 
@@ -22,5 +27,6 @@ $this->setFrameMode(true);
         </div>
     </div>
     <div class="review-img-wrap"><a href="#"><img src="content/photo_1.jpg" alt="img"></a></div>
+</div>
 </div>
 <?endforeach;?>
